bef34393bf32575b1f33dd4b6f5dee87ebf1428e
jack eichner <jack8191@gmail.com>
2018-10-19 20:22:50 -0700

    trying again to get this online

---
+++ index.js
@@ -0,0 +1,51 @@
+//we need an event listener function for a form submission
+//we need a function to take the user-inputted text and search youtube's api
+//we need a function to render the result to the DOM
+
+//obtain data with search word from API
+function getDataFromApi(searchTerm, callback) {
+    let url = 'https://www.googleapis.com/youtube/v3/search';
+    let params = {
+        part: 'snippet',
+        key: 'AIzaSyCcFpj2Cb42RpVtN6rpE5K2jH2N2wTgeKI',
+        maxResults: 25,
+        q: searchTerm,
+        type:'video'
+    };
+  
+    $.getJSON(url, params, callback);
+
+}
+
+
+//Spit result into HTML
+function renderResult() {
+    return `
+    <div class="js-search-results">
+        <img src="${items.snippet.thumbnails.medium.url}"> 
+    </div>
+    `;
+
+}
+
+function displayYoutubeSearchData() {
+    const results = items.map((items, _index) => renderResult(items));
+    $('.js-search-results').html(results);
+  }
+
+
+//Event listener for form submission
+function watchSubmit() {
+    $('.js-search-form').submit(event => {
+        event.preventDefault();
+        const queryTarget = $(event.currentTarget).find('.js-query');
+        const query = queryTarget.val();
+        // clear out the input
+        queryTarget.val("");
+        getDataFromApi(query, displayYoutubeSearchData);
+      });
+
+}
+
+$(watchSubmit);
+
